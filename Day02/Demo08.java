b36fa42aa86996bccccd357c409f6823b9aa6dea
bryan <755230452@qq.com>
2020-10-07 18:29:57 +0800

    javaDay02

--- Day02/Demo08.java
+++ Day02/Demo08.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Test{
+public class Demo08{
     public static void main(String[] args){
         
         System.out.println("请输入总钱数:");
@@ -11,26 +11,26 @@ public class Test{
 
         //总1美元数
         int dollarSum = pennyAllSum/100;
-        int pennySum = pennyAllSum%100;
+        pennyAllSum = pennyAllSum%100;
 
         //总2角5分数
-        int quarterSum = pennySum/25;
-        int pennySum1 = pennySum%25;
+        int quarterSum = pennyAllSum/25;
+        pennyAllSum = pennyAllSum%25;
 
         //总1角数
-        int dimeSum = pennySum1/10;
-        int pennySum2 = pennySum1%10;
+        int dimeSum = pennyAllSum/10;
+        pennyAllSum = pennyAllSum%10;
 
         //总5分数
-        int nickel = pennySum2/5;
+        int nickel = pennyAllSum/5;
         //总1分数
-        int pennySum3 = pennySum2%5;
+        pennyAllSum = pennyAllSum%5;
 
         System.out.println(dollarSum+"个1美元");
         System.out.println(quarterSum+"个2角5分");
         System.out.println(dimeSum+"个1角");
         System.out.println(nickel+"个5分");
-        System.out.println(pennySum3+"个1分");
+        System.out.println(pennyAllSum+"个1分");
 
     }
 }
